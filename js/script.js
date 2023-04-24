c0723f00c31c2b6bd100fab6684a5aefee1fa1cb
RainbowPlayer <kolak375@gmail.com>
2023-04-24 14:08:08 +0300

    new page

---
+++ js/script.js
@@ -0,0 +1,15 @@
+    $(document).ready(function(){
+
+    $('.hide_check').click(function(){
+        $('.nav').addClass('active-n');
+    })
+
+    $('.nav .close').click(function(){
+        console.log(this);
+        console.log(event.target);
+        if(this === event.target){
+            console.log("our target");
+            $('.nav').removeClass('active-n');
+        }
+    })
+});
