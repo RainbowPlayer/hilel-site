8f3a7a30841c968cd83c36759df1e5c6176da63d
RainbowPlayer <kolak375@gmail.com>
2023-04-25 16:38:16 +0300

    new page

--- js/script.js
+++ js/script.js
@@ -13,6 +13,32 @@
         }
     })
 
+    $('#customer').click(function(){
+        $('#customer').addClass('ss-active');
+        $('.customer-text').addClass('ss-active-text');
+        $('#consistent, #innovative, #business').removeClass('ss-active');
+        $('.consistent-text, .innovative-text, .business-text').removeClass('ss-active-text')
+    })
 
+    $('#consistent').click(function(){
+        $('#consistent').addClass('ss-active');
+        $('.consistent-text').addClass('ss-active-text');
+        $('#customer, #innovative, #business').removeClass('ss-active');
+        $('.customer-text, .innovative-text, .business-text').removeClass('ss-active-text')
+    })
+
+    $('#innovative').click(function(){
+        $('#innovative').addClass('ss-active');
+        $('.innovative-text').addClass('ss-active-text');
+        $('#customer, #consistent, #business').removeClass('ss-active');
+        $('.customer-text, .consistent-text, .business-text').removeClass('ss-active-text')
+    })
+
+    $('#business').click(function(){
+        $('#business').addClass('ss-active');
+        $('.business-text').addClass('ss-active-text');
+        $('#customer, #consistent, #innovative').removeClass('ss-active');
+        $('.customer-text, .innovative-text, .consistent-text').removeClass('ss-active-text')
+    })
     
 });
