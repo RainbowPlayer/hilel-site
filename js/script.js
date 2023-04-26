cbec2cc9751e20fe5673978e3c06593e79be4ddd
RainbowPlayer <kolak375@gmail.com>
2023-04-26 13:47:04 +0300

    new page

--- js/script.js
+++ js/script.js
@@ -13,32 +13,34 @@
         }
     })
 
-    $('#customer').click(function(){
-        $('#customer').addClass('ss-active');
-        $('.customer-text').addClass('ss-active-text');
-        $('#consistent, #innovative, #business').removeClass('ss-active');
-        $('.consistent-text, .innovative-text, .business-text').removeClass('ss-active-text')
-    })
 
-    $('#consistent').click(function(){
-        $('#consistent').addClass('ss-active');
-        $('.consistent-text').addClass('ss-active-text');
-        $('#customer, #innovative, #business').removeClass('ss-active');
-        $('.customer-text, .innovative-text, .business-text').removeClass('ss-active-text')
-    })
-
-    $('#innovative').click(function(){
-        $('#innovative').addClass('ss-active');
-        $('.innovative-text').addClass('ss-active-text');
-        $('#customer, #consistent, #business').removeClass('ss-active');
-        $('.customer-text, .consistent-text, .business-text').removeClass('ss-active-text')
-    })
-
-    $('#business').click(function(){
-        $('#business').addClass('ss-active');
-        $('.business-text').addClass('ss-active-text');
-        $('#customer, #consistent, #innovative').removeClass('ss-active');
-        $('.customer-text, .innovative-text, .consistent-text').removeClass('ss-active-text')
-    })
+    $('.selection-button').click(function () {
+        const target = this;
+        const id = $(this).data('target');
     
+        $('.selection-button').each(function (index, button) {
+          if (button === target) {
+            $(button).addClass('ss-active');
+          } else {
+            $(button).removeClass('ss-active');
+          }
+        });
+    
+        $('.ss-tx').each(function (index, tab) {
+          if ($(tab).attr('id') === id) {
+            $(tab).addClass('ss-active-text');
+          } else {
+            $(tab).removeClass('ss-active-text');
+          }
+        });
+      });
+      
 });
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+  
